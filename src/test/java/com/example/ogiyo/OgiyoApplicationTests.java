409f35ebc5a68bb2ca027b40def735d4eb85fec3
khw00185 <ksng0185@naver.com>
2025-02-28 20:47:00 +0900

    feat: init

---
+++ src/test/java/com/example/ogiyo/OgiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ogiyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgiyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
