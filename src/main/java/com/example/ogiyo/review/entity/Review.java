66ff1121df10fdedf2d0dba08fb1a11ff15f49d4
khw00185 <ksng0185@naver.com>
2025-02-28 23:40:33 +0900

    feat: Review 엔티티, repository 생성

--- src/main/java/com/example/ogiyo/review/entity/Review.java
+++ src/main/java/com/example/ogiyo/review/entity/Review.java
@@ -1,19 +1,36 @@
-package com.example.ogiyo.review.entity;
+package com.example.ogiyo.reviews.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.ogiyo.common.BaseEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Review {
+@Table(name = "reviews")
+public class Reviews extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "storesId", nullable = false)
+    private Stores stores;  // 가게 ID (외래키)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usersId", nullable = false)
+    private Users users;  // 회원 ID (외래키)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menusId")
+    private Menus menus;  // 메뉴 ID (NULL 가능)
+
+    @Column(nullable = false)
+    private byte rating;  // 별점
+
+    @Column(nullable = false, length = 255)
+    private String content;  // 내용
 
 
 }
