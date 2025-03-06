9f48d918354aae95c872de4b9e6b770de92c0c5c
Yujun <pathfinder357@github.com>
2025-03-06 10:36:23 +0900

    aa

--- src/main/java/com/example/ogiyo/coupon/repository/CouponRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ogiyo.coupon.repository;
-
-import com.example.ogiyo.coupon.entity.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-}
